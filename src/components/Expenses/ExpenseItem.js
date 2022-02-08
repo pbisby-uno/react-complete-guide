da9649ab9c0137e0b27670e7b2636de19614fd1f
pbisby-uno <philip.bisby@unosquare.com>
2022-02-08 18:36:21 +0000

    add expense filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,16 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
+import React from "react";
+
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>
-          <div>{props.amount}</div>
-        </div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
